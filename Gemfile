829421fa7df4b58bf0bb5665731d8b76ea4e34e9
4ndeez <andissteam@gmail.com>
2021-07-20 18:02:25 +0300

    add tags to user model

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 
+gem 'acts-as-taggable-on', '~> 6.0'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  acts_as_taggable_on :tags
+  before_save :downcase_tags
+
   validates :first_name, :last_name, :id_number, presence: true, length: { in: 2..26 }
   validates :middle_name, presence: false
   validates :id_number, uniqueness: true, numericality: { only_integer: true }
+
+  def downcase_tags
+    self.tag_list.map!(&:downcase)
+  end
 end
